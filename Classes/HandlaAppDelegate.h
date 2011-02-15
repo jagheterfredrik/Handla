6b30f35df1448d46075bdf779e5d0529985e4985
Fredrik Gustafsson <frgustaf@kth.se>
2011-02-15 09:57:54 +0100

    Initial commit

---
+++ Classes/HandlaAppDelegate.h
@@ -0,0 +1,32 @@
+//
+//  HandlaAppDelegate.h
+//  Handla
+//
+//  Created by Fredrik Gustafsson on 2011-02-15.
+//  Copyright 2011 Kungliga Tekniska Högskolan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface HandlaAppDelegate : NSObject <UIApplicationDelegate> {
+    
+    UIWindow *window;
+    
+@private
+    NSManagedObjectContext *managedObjectContext_;
+    NSManagedObjectModel *managedObjectModel_;
+    NSPersistentStoreCoordinator *persistentStoreCoordinator_;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (NSURL *)applicationDocumentsDirectory;
+- (void)saveContext;
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Handla
+//
+//  Created by Fredrik Gustafsson on 2011-02-15.
+//  Copyright 2011 Kungliga Tekniska Högskolan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
