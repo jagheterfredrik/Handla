95b6574ae7199383f7c4747d91772f9be3e0606c
Fredrik Gustafsson <fredrikg@n157-p203.kthopen.kth.se>
2011-03-23 12:59:39 +0100

    FPS-fix. Ny UISwitch

--- Classes/PDColoredProgressView.m
+++ Classes/PDColoredProgressView.m
@@ -14,11 +14,11 @@
 {
     if (self.progress < _targetProgress)
 	{
-        self.progress = MIN(self.progress + 0.02, _targetProgress);
+        self.progress = MIN(self.progress + 0.04, _targetProgress);
     }
     else if(self.progress > _targetProgress)
     {
-        self.progress = MAX(self.progress - 0.02, _targetProgress);
+        self.progress = MAX(self.progress - 0.04, _targetProgress);
     }
     else
 	{
@@ -34,7 +34,7 @@
         _targetProgress = newProgress;
         if (_progressTimer == nil)
 		{
-            _progressTimer = [NSTimer scheduledTimerWithTimeInterval:0.01 target:self selector:@selector(moveProgress) userInfo:nil repeats:YES];
+            _progressTimer = [NSTimer scheduledTimerWithTimeInterval:0.02 target:self selector:@selector(moveProgress) userInfo:nil repeats:YES];
         }
     }
 	else
