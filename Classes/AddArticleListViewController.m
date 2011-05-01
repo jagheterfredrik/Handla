5246278024f54a05dbe1434d355ab1b05833ea8d
rutchkiwi <vho@kth.se>
2011-05-01 23:14:18 +0200

    fixar #63

--- Classes/AddArticleListViewController.m
+++ Classes/AddArticleListViewController.m
@@ -194,9 +194,13 @@
 
 
 - (NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView {
-    return [NSArray arrayWithArray:
+    if (super.searchDisplayController.active){
+        return nil;
+    } else {
+        return [NSArray arrayWithArray:
                                  [@"#|A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z|Å|Ä|Ö"
                                   componentsSeparatedByString:@"|"]];
+    }
 }
 
 
