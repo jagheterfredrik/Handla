630b71f84062c5035a3e637ac5481276c4a1752d
rutchkiwi <vho@kth.se>
2011-05-12 18:27:33 +0200

    fixar bugg med gröna blinket då man tog bort saker

--- Classes/IndividualListTableViewCell.m
+++ Classes/IndividualListTableViewCell.m
@@ -99,16 +99,24 @@
 }
 
 
+-(void)wasChecked{
+    shouldFlash++;  
+}
+
+
 - (void)layoutSubviews { 
     [super layoutSubviews]; 
     if ([self.listArticle.checked boolValue]) {
-        [self setBackgroundColor:[UIColor colorWithRed:0.18f green:0.93f blue:0.29f alpha:1.0f]];
-        [UIView beginAnimations:nil context:NULL];
-        [UIView setAnimationDuration:0.2f];
-        [self setBackgroundColor:[UIColor colorWithRed:0.97f green:1.0f blue:0.97f alpha:1.f]];
+        if (shouldFlash){//self.backgroundColor!=[UIColor colorWithRed:0.97f green:1.0f blue:0.97f alpha:1.f]) {
+            [self setBackgroundColor:[UIColor colorWithRed:0.18f green:0.93f blue:0.29f alpha:1.0f]];
+            [UIView beginAnimations:nil context:NULL];
+            [UIView setAnimationDuration:0.2f];
+            [self setBackgroundColor:[UIColor colorWithRed:0.97f green:1.0f blue:0.97f alpha:1.f]];
 
-        [UIView commitAnimations];
-            } else{
+            [UIView commitAnimations];
+            shouldFlash--;
+        }
+        } else{
         [self setBackgroundColor:[UIColor whiteColor]];
     }
 }
